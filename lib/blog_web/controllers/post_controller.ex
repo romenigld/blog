0e58b81e26365312befddfb08b985943accdb4f9
Romenig Lima Damasio <romenigld@gmail.com>
2021-06-26 19:03:25 +0200

    2 - Fundamentos: Bora Codar - Views e Templates

--- lib/blog_web/controllers/post_controller.ex
+++ lib/blog_web/controllers/post_controller.ex
@@ -2,6 +2,18 @@ defmodule BlogWeb.PostController do
   use BlogWeb, :controller
 
   def index(conn, params) do
+    posts = [
+      %{
+        title: "Phoenix"
+      },
+      %{
+        title: "LiveView"
+      },
+      %{
+        title: "PostgreSQL"
+      }
+    ]
 
+    render(conn, "index.html", posts: posts, valor: 2)
   end
 end
