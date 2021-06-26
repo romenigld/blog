5895c6d69193223d43c83aad8563008e97c5ece7
Romenig Lima Damasio <romenigld@gmail.com>
2021-06-26 20:53:52 +0200

    2 - Fundamentos: Bora Codar - Integração Postgres e Elixir

--- lib/blog_web/controllers/post_controller.ex
+++ lib/blog_web/controllers/post_controller.ex
@@ -1,26 +1,14 @@
 defmodule BlogWeb.PostController do
   use BlogWeb, :controller
+  alias Blog.Posts.Post
 
   def index(conn, params) do
-    posts = [
-      %{
-        id: 1,
-        title: "Phoenix"
-      },
-      %{
-        id: 2,
-        title: "LiveView"
-      },
-      %{
-        id: 3,
-        title: "PostgreSQL"
-      }
-    ]
-
+    posts = Blog.Repo.all(Post)
     render(conn, "index.html", posts: posts, valor: 2)
   end
 
-  def show(conn, params) do
-    render(conn, "show.html")
+  def show(conn, %{"id" => id}) do
+    post = Blog.Repo.get!(Post, id)
+    render(conn, "show.html", post: post)
   end
 end
