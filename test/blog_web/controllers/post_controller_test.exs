bef5f4da849d21c353c2149eafcbd66bb7aa4982
Romenig Lima Damasio <romenigld@gmail.com>
2021-06-29 12:44:16 +0200

    2 - Fundamentos: Garantindo Qualidade com Testes - Testes com muitas rotas

--- test/blog_web/controllers/post_controller_test.exs
+++ test/blog_web/controllers/post_controller_test.exs
@@ -11,4 +11,24 @@ defmodule BlogWeb.PostControllerTest do
     conn = get(conn, Routes.post_path(conn, :index))
     assert html_response(conn, 200) =~ "Phoenix Framework"
   end
+
+  test "pegar um post por id", %{conn: conn} do
+    {:ok, post} = Blog.Posts.create_post(@valid_post)
+    conn = get(conn, Routes.post_path(conn, :show, post))
+    assert html_response(conn, 200) =~ "Phoenix Framework"
+  end
+
+  test "entrar no formulário de criação de posts", %{conn: conn} do
+    conn = get(conn, Routes.post_path(conn, :new))
+    assert html_response(conn, 200) =~ "Criar Post"
+  end
+
+  test "criar um posts", %{conn: conn} do
+    conn = post(conn, Routes.post_path(conn, :create), post: @valid_post)
+    assert %{id: id} = redirected_params(conn)
+    assert redirected_to(conn) == Routes.post_path(conn, :show, id)
+
+    conn = get(conn, Routes.post_path(conn, :show, id))
+    assert html_response(conn, 200) =~ "Phoenix Framework"
+  end
 end
