a4fb2155c43a1f0fe80a98c477037bce34c93625
Romenig Lima Damasio <romenigld@gmail.com>
2021-07-01 00:41:12 +0200

    2 - Fundamentos: Websockets - Incluindo comentarios no socket

--- lib/blog_web/channels/comments_channel.ex
+++ lib/blog_web/channels/comments_channel.ex
@@ -2,11 +2,8 @@ defmodule BlogWeb.CommentsChannel do
   use BlogWeb, :channel
 
   def join("comments:"<> post_id, payload, socket) do
-    IO.inspect post_id, label: "post_id"
-    IO.inspect payload, label: "Payload"
-    IO.inspect socket, label: "Socket"
-
-    {:ok, %{nome: "Comments Channel deu certo!"}, socket}
+    post = Blog.Posts.get_post_with_comments!(post_id)
+    {:ok, %{comments: post.comments}, socket}
   end
 
   def handle_in() do
