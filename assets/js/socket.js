58928f59b0ab80ca1f07314b7a0a038e32e6bc03
Romenig Lima Damasio <romenigld@gmail.com>
2021-07-01 11:54:16 +0200

    2 - Fundamentos: Websockets - Carregando Comentarios

--- assets/js/socket.js
+++ assets/js/socket.js
@@ -55,9 +55,13 @@ let socket = new Socket("/socket", {params: {token: window.userToken}})
 socket.connect()
 
 // Now that you are connected, you can join channels with a topic:
-let channel = socket.channel("comments:2", {})
-channel.join()
-  .receive("ok", resp => { console.log("Joined successfully", resp) })
-  .receive("error", resp => { console.log("Unable to join", resp) })
 
-export default socket
+const createSocket = (post_id) => {
+  let channel = socket.channel(`comments:${post_id}`, {})
+  channel.join()
+    .receive("ok", resp => { console.log("Joined successfully", resp) })
+    .receive("error", resp => { console.log("Unable to join", resp) })
+
+}
+
+window.createSocket = createSocket
