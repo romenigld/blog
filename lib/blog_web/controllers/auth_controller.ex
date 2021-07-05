b94cf997e763eadb0565c3795b98b4a9cb0c863a
Romenig Lima Damasio <romenigld@gmail.com>
2021-07-05 23:24:10 +0200

    criaçao de usuario

---
+++ lib/blog_web/controllers/auth_controller.ex
@@ -0,0 +1,21 @@
+defmodule BlogWeb.AuthController do
+  use BlogWeb, :controller
+
+  plug Ueberauth
+
+  def callback(%{assigns: %{ueberauth_auth: auth}} = conn, %{"provider" => provider}) do
+    IO.inspect(auth)
+    user =
+      %{
+        token: auth.credentials.token,
+        email: auth.info.email,
+        first_name: auth.info.first_name,
+        last_name: auth.info.last_name,
+        image: auth.info.image,
+        provider: provider
+      }
+    IO.inspect(user)
+
+    render(conn, "index.html")
+  end
+end
