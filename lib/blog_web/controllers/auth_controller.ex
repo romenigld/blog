24015ac10eaee872e39ab1eec5eef1d0d7fa5998
Romenig Lima Damasio <romenigld@gmail.com>
2021-07-06 00:07:21 +0200

    2 - Fundamentos: Oauth - Regra de Criacao de Usuario

--- lib/blog_web/controllers/auth_controller.ex
+++ lib/blog_web/controllers/auth_controller.ex
@@ -1,5 +1,6 @@
 defmodule BlogWeb.AuthController do
   use BlogWeb, :controller
+  alias Blog.Accounts
 
   plug Ueberauth
 
@@ -14,8 +15,9 @@ defmodule BlogWeb.AuthController do
         image: auth.info.image,
         provider: provider
       }
-    IO.inspect(user)
+      |> Accounts.create_user()
 
     render(conn, "index.html")
+
   end
 end
