91392d6205c5bc222173631f1aaa37c4f038d3c5
Romenig Lima Damasio <romenigld@gmail.com>
2021-07-06 23:14:13 +0200

    2 - Fundamentos: Plugs - Refatorando Testes( Testes com usuário na seção )

--- test/blog_web/controllers/post_controller_test.exs
+++ test/blog_web/controllers/post_controller_test.exs
@@ -18,7 +18,10 @@ defmodule BlogWeb.PostControllerTest do
   end
 
   test "entrar no formulário de criação de posts", %{conn: conn} do
-    conn = get(conn, Routes.post_path(conn, :new))
+    conn =
+      conn
+      |> Plug.Test.init_test_session(user_id: 1)
+      |> get(Routes.post_path(conn, :new))
     assert html_response(conn, 200) =~ "Criar Post"
   end
 
@@ -36,7 +39,10 @@ defmodule BlogWeb.PostControllerTest do
   end
 
   test "criar um posts com valores inválidos", %{conn: conn} do
-    conn = post(conn, Routes.post_path(conn, :create), post: %{})
+    conn =
+      conn
+      |> Plug.Test.init_test_session(user_id: 1)
+      |> post(Routes.post_path(conn, :create), post: %{})
     assert html_response(conn, 200) =~ "campo obrigatório"
   end
 
@@ -59,28 +65,41 @@ defmodule BlogWeb.PostControllerTest do
 
     test "entrar no formulário de alteração de posts", %{conn: conn, post: post} do
       user = Blog.Accounts.get_user!(1)
-      {:ok, post } = Blog.Posts.create_post(user, @valid_post)
-      conn = get(conn, Routes.post_path(conn, :edit, post))
+      {:ok, post} = Blog.Posts.create_post(user, @valid_post)
+      conn =
+        conn
+        |> Plug.Test.init_test_session(user_id: 1)
+        |> get(conn, Routes.post_path(conn, :edit, post))
       assert html_response(conn, 200) =~ "Editar Post"
     end
 
     test "alterar um post", %{conn: conn, post: post} do
-      conn = put(conn, Routes.post_path(conn, :update, post), post: @update_post)
+      conn =
+        conn
+        |> Plug.Test.init_test_session(user_id: 1)
+        |> put(Routes.post_path(conn, :update, post), post: @update_post)
+
       assert %{id: id} = redirected_params(conn)
       assert redirected_to(conn) == Routes.post_path(conn, :show, id)
+
       conn = get(conn, Routes.post_path(conn, :show, id))
       assert html_response(conn, 200) =~ "Update 123"
     end
 
     test "alterar um posts com valores inválidos", %{conn: conn, post: post} do
       conn =
-        put(conn, Routes.post_path(conn, :update, post), post: %{title: nil, description: nil})
+        conn
+        |> Plug.Test.init_test_session(user_id: 1)
+        |> put(Routes.post_path(conn, :update, post), post: %{title: nil, description: nil})
 
       assert html_response(conn, 200) =~ "campo obrigatório"
     end
 
     test "delete post", %{conn: conn, post: post} do
-      conn = delete(conn, Routes.post_path(conn, :delete, post))
+      conn =
+        conn
+        |> Plug.Test.init_test_session(user_id: 1)
+        |> delete(Routes.post_path(conn, :delete, post))
       assert redirected_to(conn) == Routes.post_path(conn, :index)
       assert_error_sent 404, fn -> get(conn, Routes.post_path(conn, :show, post)) end
     end
