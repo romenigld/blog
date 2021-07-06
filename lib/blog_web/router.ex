13a6a11c448edfea8c0dcb9f16e6b4516a6f51d9
Romenig Lima Damasio <romenigld@gmail.com>
2021-07-06 12:58:03 +0200

    2 - Fundamentos: Plugs - Configurando Plug

--- lib/blog_web/router.ex
+++ lib/blog_web/router.ex
@@ -7,6 +7,7 @@ defmodule BlogWeb.Router do
     plug :fetch_flash
     plug :protect_from_forgery
     plug :put_secure_browser_headers
+    plug BlogWeb.Plug.SetUser
   end
 
   # coveralls-ignore-start
